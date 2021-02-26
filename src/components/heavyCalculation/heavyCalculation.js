ce407dde75893450099cd725717e407ff556b267
Daniel Schulz <hallo@iamschulz.de>
2021-02-26 12:02:16 +0100

    playing around with DOM implementations in worker

--- src/components/heavyCalculation/heavyCalculation.js
+++ src/components/heavyCalculation/heavyCalculation.js
@@ -1,44 +1,63 @@
-import Component from '../../helpers/component';
-import HeavyCalculationWorker from './heavyCalculation.worker';
-import './heavyCalculation.scss';
+import Component from "../../helpers/component";
+import HeavyCalculationWorker from "./heavyCalculation.worker";
+import "./heavyCalculation.scss";
 
 export default class HeavyCalculation extends Component {
-    prepare() {
-        const parsedCurrent = parseInt(this.calculatedNumber.innerText, 10);
-        this.current = typeof (parsedCurrent) !== 'number' ? parsedCurrent : 0;
-        this.finish = 999999999;
-        this.counter = new HeavyCalculationWorker();
+	prepare() {
+		const parsedCurrent = parseInt(this.calculatedNumber.innerText, 10);
+		this.current = typeof parsedCurrent !== "number" ? parsedCurrent : 0;
+		this.finish = 999999999;
+		this.counter = new HeavyCalculationWorker();
 
-        this.StateMachine = new StateMachine(this, {
-            calculation: {
-                progress: {
-                    event: 'heavyCaluclationProgress',
-                },
-                finished: {
-                    event: 'heavyCaluclationFinished',
-                    on: 'updateNumber',
-                },
-            },
-        });
-    }
+		this.StateMachine = new StateMachine(this, {
+			calculation: {
+				progress: {
+					event: "heavyCaluclationProgress",
+				},
+				finished: {
+					event: "heavyCaluclationFinished",
+					on: "updateNumber",
+				},
+			},
+		});
+	}
 
-    init() {
-        EventBus.publish('heavyCaluclationProgress', this.el);
-        this.counter.postMessage({
-            count: {
-                from: this.current,
-                to: this.finish,
-            },
-        });
+	init() {
+		EventBus.publish("heavyCaluclationProgress", this.el);
 
-        this.counter.addEventListener('message', (event) => {
-            if (event.data.finished) {
-                EventBus.publish('heavyCaluclationFinished', this.el);
-            }
-        });
-    }
+		const Elem = (e) => {
+			return {
+				toJSON: () => ({
+					tagName: e.tagName,
+					textContent: e.textContent,
+					attributes: Array.from(e.attributes).map((a) => ({
+						name: a.name,
+						value: a.value,
+					})),
+					children: Array.from(e.children, Elem),
+				}),
+			};
+		};
 
-    updateNumber() {
-        this.calculatedNumber.innerText = this.finish;
-    }
+		// html2json :: Node -> JSONString
+		const html2json = (e) => JSON.stringify(Elem(e), null, "  ");
+
+		this.counter.postMessage({
+			count: {
+				from: this.current,
+				to: this.finish,
+			},
+			html: html2json(document.body),
+		});
+
+		this.counter.addEventListener("message", (event) => {
+			if (event.data.finished) {
+				EventBus.publish("heavyCaluclationFinished", this.el);
+			}
+		});
+	}
+
+	updateNumber() {
+		this.calculatedNumber.innerText = this.finish;
+	}
 }
--- src/components/heavyCalculation/heavyCalculation.worker.js
+++ src/components/heavyCalculation/heavyCalculation.worker.js
@@ -1,18 +1,109 @@
 // need self in workers
 /* eslint-disable no-restricted-globals */
 const count = (from, to) => {
-    for (let current = from; current <= to; current++) {
-        if (current === to) {
-            self.postMessage({ finished: true });
-        }
-    }
+	for (let current = from; current <= to; current++) {
+		if (current === to) {
+			self.postMessage({ finished: true });
+		}
+	}
 };
 
-self.addEventListener('message', (event) => {
-    if (event.data.count) {
-        count(
-            event.data.count.from ? event.data.count.from : 0,
-            event.data.count.to ? event.data.count.to : 0
-        );
-    }
+self.addEventListener("message", (event) => {
+	const virtualDom = JSON.parse(event.data.html);
+
+	const findElements = (needle, haystack) => {
+		const found = [];
+
+		const splitSelector = (selector) => {
+			const selectorList = [];
+
+			selector.split(" ").forEach((level) => {
+				const regex = new RegExp(
+					/(?:\.(?<class>\w+))|(?:#(?<id>\w+))|(?:\[(?<attr>.*)\])|(?<tag>\w+)/gs
+				);
+				const match = [...level.matchAll(regex)];
+
+				selectorList.push({
+					tagName: Array.from(match).map((x) => x.groups.tag)[0],
+					classList: Array.from(match).map((x) => x.groups.class),
+					id: Array.from(match).map((x) => x.groups.id)[0],
+					attrList: Array.from(match).map((x) => x.groups.attr),
+				});
+			});
+
+			return selectorList;
+		};
+
+		const matchesTagName = (sel, el) => {
+			return sel.tagName
+				? el.tagName.toLowerCase() === sel.tagName.toLowerCase()
+				: false;
+		};
+
+		const matchesClassList = (sel, el) => {
+			const elClasses = el.attributes
+				.filter((attr) => attr.name === "class")
+				.shift();
+
+			return sel.classList && elClasses
+				? sel.classList.every((x) =>
+						elClasses.value.split(" ").includes(x)
+				  )
+				: false;
+		};
+
+		const matchesId = (sel, el) => {
+			return sel.id
+				? el.id.toLowerCase() === sel.id.toLowerCase()
+				: false;
+		};
+
+		matchesAttrList = (sel, el) => {
+			// todo: doesnt match correctly yet
+			const elAttributes = el.attributes
+				.filter((attr) => attr.name === "class")
+				.shift();
+
+			return sel.attrList && elAttributes
+				? sel.attrList.every((x) =>
+						elAttributes.value.split(" ").includes(x)
+				  )
+				: false;
+		};
+
+		const recursiveFindIn = (elements) => {
+			elements.forEach((element) => {
+				const selector = splitSelector(needle)[0];
+
+				if (
+					matchesTagName(selector, element) ||
+					matchesClassList(selector, element) ||
+					matchesId(selector, element) ||
+					matchesAttrList(selector, element)
+				) {
+					found.push(element);
+				}
+
+				if (element.children) {
+					recursiveFindIn(element.children);
+				}
+			});
+		};
+
+		recursiveFindIn(haystack);
+		return found;
+	};
+
+	const header = findElements(
+		'[data-component="header"][data-state-menu-button="inactive"]',
+		virtualDom.children
+	);
+	console.log(header);
+
+	if (event.data.count) {
+		count(
+			event.data.count.from ? event.data.count.from : 0,
+			event.data.count.to ? event.data.count.to : 0
+		);
+	}
 });
